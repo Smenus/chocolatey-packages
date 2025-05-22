ea511566b2bced13a65ed67a6846cb5968932a46
Sean Fleming <smenus@me.com>
2025-05-22 14:39:32 +0000

    AU: 1 updated - nsis-advancedlogging [skip ci] https://gist.github.com/Smenus/569565b2530f1c91a5cad329673d9309/4ecb1c707f39b680e1ceb1d70ceb3bbd73ea8e4e

--- nsis-advancedlogging/tools/chocolateyInstall.ps1
+++ nsis-advancedlogging/tools/chocolateyInstall.ps1
@@ -6,9 +6,9 @@ if (!$nsisDir)  { throw "Can't find NSIS install location"; }
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://iweb.dl.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04-log.zip'
-  checksum      = 'e33bd03a3deb056bc217da6c929a6ad1b41ca07de6ac34574f14fbcb64d87039'
-  checksumType  = 'sha256'
+  url           = 'https://sourceforge.net/projects/nsis/files/NSIS%203/3.11/nsis-3.11-log.zip/download'
+  checksum      = '86044819c632da4e92ded1deeb97fff9'
+  checksumType  = 'md5'
 
   unzipLocation = $nsisDir
 }
